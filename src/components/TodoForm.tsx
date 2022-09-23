0d24f84b75b913997d5bc6373fdd3f925755b904
Martin AUBEUT <martin.aubeut@gmail.com>
2022-09-23 20:09:30 +0200

    Add form className

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -22,12 +22,14 @@ const handleSubmit = async(e: any) => {
 };
 return (
     <FormGroup onSubmit={handleSubmit}>
+        <div className="form">
         <div className="input_container">
-        <TextField type="text" id="standard-basic" label="Add a new" variant="outlined" value={title} onChange= {(e:any) => setTitle(e.target.value)} />
+        <TextField type="text" id="standard-basic" label="Add a new task" variant="outlined" value={title} onChange= {(e:any) => setTitle(e.target.value)} />
         </div>
             <div className="btn_container">
                 <Button variant="outlined" onClick={handleSubmit}>Add</Button>
             </div>
+        </div>
         </FormGroup>
 );
 
