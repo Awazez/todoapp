53d0b88d642fff6da09fe93303a8254ac9884f0e
Martin AUBEUT <martin.aubeut@gmail.com>
2022-09-23 16:37:03 +0200

    todo edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
-import './App.css';
-import Title from './components/Title';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Title from "./components/Title";
 import TodoForm from "./components/TodoForm";
+import Todo from "./components/Todo";
+import {collection,query,onSnapshot,doc,updateDoc,deleteDoc,} from "firebase/firestore";
+import { db } from "./firebase";
+
+
+interface IProps {
+  todo: any;
+  toggleComplete: any
+  handleDelete: any
+  handleEdit: any
+}
 
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray:any = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo:any, title:any) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo:any) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id:any) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
   return (
     <div className="App">
-            <Title/>
-            <TodoForm/>
+      <div>
+        <Title />
+      </div>
+      <div>
+        <TodoForm />
+      </div>
+      <div className="todo_container">
+        {todos.map((todo) => (<Todo key={todo.id} todo={todo} toggleComplete={toggleComplete} handleDelete={handleDelete} handleEdit={handleEdit}/>
+        ))}
+      </div>
     </div>
   );
 }
-
 export default App;
