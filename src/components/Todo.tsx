7be67ea3a0dc5574a83a82de71f32ad99714d5a3
Martin AUBEUT <martin.aubeut@gmail.com>
2022-09-24 17:49:18 +0200

    Add css task

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -25,7 +25,7 @@ export default function Todo({ todo, toggleComplete, handleDelete, handleEdit,}:
   };
   return (
     <div className="todo">
-      <input type="text" value={todo.title === "" ? newTitle : todo.title} className="task-box" onChange={handleChange}/>
+      <input id="todo-input" type="text" value={todo.title === "" ? newTitle : todo.title} className="task-box" onChange={handleChange}/>
       <div>
         <button className="button-complete" onClick={() => toggleComplete(todo)}>
           <CheckCircleIcon id="i" />
