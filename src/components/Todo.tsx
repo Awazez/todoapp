4c55de0343e8e8e21ba36d23dfb9ced6b82eacb0
Martin AUBEUT <martin.aubeut@gmail.com>
2022-09-26 12:38:40 +0200

    delete react import

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React ,{useState, ChangeEvent} from "react";
+import {useState, ChangeEvent} from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
